f3cd18345c095afc96b171eda4bc7dde35ba3feb
Corey Gelbaugh <gelbaughcm@gmail.com>
2022-09-16 10:33:36 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rainbow';
+  // colors = [
+  //   //'#7d4138',
+  //   'maroon',
+  //   'red',
+  //   'orangered',
+  //   //'#ff8c00',
+
+  //   'orange',
+  //   '#FFD580',
+  //   'yellow',
+  //   'limegreen',
+  //   'green',
+  //   //'teal',
+  //   //'#83A1CD',
+  //   //'#00FFFF',
+  //   '#96DED1',
+  //   'blue',
+  //   'indigo',
+  //   //'purple',
+  //   '#FF00FF',
+  //   'violet',
+  //   //'#FF6961',
+  // ];
+  colors = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'indigo',
+    'violet',
+  ];
+  totalPx = 1000;
+
+  get increment(): number {
+    return this.totalPx / this.colors.length;
+  }
 }
